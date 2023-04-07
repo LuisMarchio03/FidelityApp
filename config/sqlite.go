5badab0692b0018064289a72f2cb1265bcb98da7
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-07 17:11:09 -0300

    feat: add sqlite config logic

---
+++ config/sqlite.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"os"
+
+	"github.com/LuisMarchio03/acim-backend/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSqlite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.go"
+
+	// Check if the database file exists
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("database file not found, creating...")
+		// Create the database file and directory
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+
+	// create db and connect
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+
+	// Migrate the schema
+
+	// User
+	err = db.AutoMigrate(&schemas.User{})
+	if err != nil {
+		logger.Errorf("sqlite automigrate error: %v", err)
+		return nil, err
+	}
+
+	// Company
+	err = db.AutoMigrate(&schemas.Company{})
+	if err != nil {
+		logger.Errorf("sqlite automigrate error: %v", err)
+		return nil, err
+	}
+
+	// Return the DB
+	return db, nil
+}
