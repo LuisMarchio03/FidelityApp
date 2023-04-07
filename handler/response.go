6343a83ae69753a7f4af7807db6cd9cfd418f92a
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-07 17:35:07 -0300

    :sparkles: add request json helper

---
+++ handler/response.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"fmt"
+
+	"github.com/LuisMarchio03/acim-backend/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(ctx *gin.Context, code int, msg string) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message":   msg,
+		"errorCode": code,
+	})
+}
+
+func sendSuccess(ctx *gin.Context, code int, op string, data interface{}) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message":     fmt.Sprintf("operation from handler: %s successfull", op),
+		"successCode": code,
+		"data":        data,
+	})
+}
+
+type ErrorResponse struct {
+	Message   string `json:"message"`
+	ErrorCode string `json:"errorCode"`
+}
+
+type ListCompaniesResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.CompanyResponse `json:"data"`
+}
