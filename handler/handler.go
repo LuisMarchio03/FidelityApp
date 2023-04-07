54b22568a37fb4288faee6bb6f4b1e5835ae7eb8
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-07 17:34:35 -0300

    :sparkles: add initialize handler package

--- handler/handler.go
+++ handler/handler.go
@@ -1,4 +1,16 @@
 package handler
 
+import (
+	"github.com/LuisMarchio03/acim-backend/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
 func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
 }
