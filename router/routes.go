fc3a97ef03ea376e312dbcb48241f2431709396d
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-07 17:35:17 -0300

    :sparkles: add response json helper

--- router/routes.go
+++ router/routes.go
@@ -1,13 +1,18 @@
 package router
 
 import (
+	docs "github.com/LuisMarchio03/acim-backend/docs"
 	"github.com/LuisMarchio03/acim-backend/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitializeRoutes(r *gin.Engine) {
 	// Initialize the handlers
 	handler.InitializeHandler()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 
 	// Initialize the routes
 	v1 := r.Group("/api/v1")
@@ -16,4 +21,9 @@ func InitializeRoutes(r *gin.Engine) {
 			handler.ListCompaniesHandler(ctx)
 		})
 	}
+
+	// Intialize Swagger
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(
+		swaggerfiles.Handler,
+	))
 }
