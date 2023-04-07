2d802c3fcb78c45725a72d40d7c318fb0e8a06e2
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-07 17:34:14 -0300

    :bug: fix bug in sqlite3 config package

--- config/sqlite.go
+++ config/sqlite.go
@@ -10,7 +10,7 @@ import (
 
 func InitializeSqlite() (*gorm.DB, error) {
 	logger := GetLogger("sqlite")
-	dbPath := "./db/main.go"
+	dbPath := "./db/main.db"
 
 	// Check if the database file exists
 	_, err := os.Stat(dbPath)
