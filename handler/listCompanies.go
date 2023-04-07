db1925a70709060c67bfbf4dce76ca0df032b35e
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-07 17:35:36 -0300

    :sparkles: add list companies handler logic

--- handler/listCompanies.go
+++ handler/listCompanies.go
@@ -3,11 +3,27 @@ package handler
 import (
 	"net/http"
 
+	"github.com/LuisMarchio03/acim-backend/schemas"
 	"github.com/gin-gonic/gin"
 )
 
+// @BaseParh /api/v1
+
+// @Summary List Companies
+// @Description List all Companies
+// @Tags Opening
+// @Accept json
+// @Producer json
+// @Success 200 {object} ListCompaniesResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /companies [get]
 func ListCompaniesHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+	companies := []schemas.Company{}
+
+	if err := db.Find(&companies).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing companies")
+	}
+
+	sendSuccess(ctx, http.StatusOK, "list-companies", companies)
 }
