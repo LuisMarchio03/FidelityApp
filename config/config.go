df5d4381035849f018d0abe7b8e832184c50cf22
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-07 17:11:46 -0300

    :sparkles: add initialize config package

---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+	var err error
+
+	// Initialize Sqlite3
+	db, err = InitializeSqlite()
+	if err != nil {
+		return fmt.Errorf("error initializing sqlite: %v", err)
+	}
+
+	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
+}
+
+func GetLogger(p string) *Logger {
+	// Initialize Logger
+	logger = NewLogger(p)
+	return logger
+}
