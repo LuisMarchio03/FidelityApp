9a6fb2ff8e3588064f31291339985ca8de8e7a4a
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-08 11:25:36 -0300

    :sparkles: add show user handler logic

---
+++ handler/user/showUser.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/LuisMarchio03/acim-backend/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+// @BaseParh /api/v1
+
+// @Summary Show User
+// @Description Show a user
+// @Tags User
+// @Accept json
+// @Producer json
+// @Param id query string true "User identification"
+// @Success 200 {object} ShowUserResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /user [get]
+func ShowUserHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamsIsRequired("id",
+			"queryParameter").Error())
+		return
+	}
+	user := schemas.User{}
+
+	if err := db.First(&user, id).Error; err != nil {
+		sendError(
+			ctx,
+			http.StatusNotFound,
+			fmt.Sprintf("error show user with id: %s", id),
+		)
+	}
+
+	sendSuccess(
+		ctx,
+		http.StatusOK,
+		"show-user",
+		user,
+	)
+}
