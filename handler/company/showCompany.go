1e8cc562e3ca013eb2c65c5dff4a43ba030e503d
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-08 11:24:54 -0300

    :ambulance: separate show company handler logic

---
+++ handler/company/showCompany.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/LuisMarchio03/acim-backend/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+// @BaseParh /api/v1
+
+// @Summary Show Company
+// @Description Show a company
+// @Tags Company
+// @Accept json
+// @Producer json
+// @Param id query string true "Company identification"
+// @Success 200 {object} ShowCompanyResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /company [get]
+func ShowCompanyHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamsIsRequired("id",
+			"queryParameter").Error())
+		return
+	}
+	company := schemas.Company{}
+	users := []schemas.User{}
+
+	if err := db.First(&company, id).Error; err != nil {
+		sendError(
+			ctx,
+			http.StatusNotFound,
+			fmt.Sprintf("error show company with id: %s", id),
+		)
+	}
+
+	if err := db.Find(&users).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing users")
+	}
+
+	company.Users = users
+
+	sendSuccess(
+		ctx,
+		http.StatusOK,
+		"show-company",
+		company,
+	)
+}
