0195c45c406bfee89ea2b608e130e41fa7e6f7dd
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-08 11:25:09 -0300

    :sparkles: add create user handler logic

---
+++ handler/user/createUser.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/LuisMarchio03/acim-backend/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+// @BaseParh /api/v1
+
+// @Summary Create User
+// @Description Create a new user
+// @Tags User
+// @Accept json
+// @Producer json
+// @Param request body CreateUserRequest true "Request body"
+// @Success 201 {object} CreateUserResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /user [post]
+func CreateUserHandler(ctx *gin.Context) {
+	request := CreateUserRequest{}
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	user := schemas.User{
+		Name:      request.Name,
+		Email:     request.Email,
+		Address:   request.Address,
+		Phone:     request.Phone,
+		Password:  request.Password,
+		CompanyId: request.CompanyId,
+		Type:      request.Type,
+	}
+
+	if err := db.Create(&user).Error; err != nil {
+		logger.Errorf("error creating user: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating user on database")
+		return
+	}
+
+	sendSuccess(ctx, http.StatusCreated, "create-user", user)
+}
