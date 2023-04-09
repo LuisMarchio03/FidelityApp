3c051898214a7df96e2e7f852541f2222637d9ab
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-09 14:29:06 -0300

    :sparkles: add show cardFidelity handler logic

---
+++ handler/cardFidelity/showCardFidelity.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/LuisMarchio03/acim-backend/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+// @BaseParh /api/v1
+
+// @Summary Show CardFidelity
+// @Description Show a cardFidelity
+// @Tags CardFidelity
+// @Accept json
+// @Producer json
+// @Param id query string true "CardFidelity identification"
+// @Success 200 {object} ShowCardFidelityResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /cardFidelity [get]
+func ShowCardFidelityHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamsIsRequired("id",
+			"queryParameter").Error())
+		return
+	}
+	cardFidelity := schemas.CardFidelity{}
+	points := []schemas.Point{}
+
+	if err := db.First(&cardFidelity, id).Error; err != nil {
+		sendError(
+			ctx,
+			http.StatusNotFound,
+			fmt.Sprintf("error show cardFidelity with id: %s", id),
+		)
+		return
+	}
+
+	if err := db.Find(&points).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing points")
+		return
+	}
+
+	cardFidelity.Point = points
+
+	sendSuccess(
+		ctx,
+		http.StatusOK,
+		"show-cardFidelity",
+		cardFidelity,
+	)
+}
