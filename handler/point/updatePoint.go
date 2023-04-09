6527fa51ccad043e3ce5f257e66d677255fffb7f
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-09 14:29:30 -0300

    :sparkles: add update point handler logic

---
+++ handler/point/updatePoint.go
@@ -0,0 +1,71 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/LuisMarchio03/acim-backend/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+// @BaseParh /api/v1
+
+// @Summary Update Point
+// @Description Update a new point
+// @Tags Point
+// @Accept json
+// @Producer json
+// @Param id query string true "Point identification"
+// @Param request body UpdatePointRequest true "Request body"
+// @Success 201 {object} UpdatePointResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /point [put]
+func UpdatePointHandler(ctx *gin.Context) {
+	// Get Request
+	request := UpdatePointRequest{}
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Get id
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamsIsRequired("id",
+			"queryParameter").Error())
+		return
+	}
+
+	point := schemas.Point{
+		CardFidelityId: request.CardFidelityId,
+		Point:          1,
+	}
+
+	// Find
+	if err := db.First(&point, id).Error; err != nil {
+		sendError(
+			ctx,
+			http.StatusNotFound,
+			fmt.Sprintf("error update point with id: %s", id),
+		)
+	}
+
+	// Update
+	if request.CardFidelityId <= 0 {
+		point.CardFidelityId = request.CardFidelityId
+	}
+
+	// Save
+	if err := db.Save(&point).Error; err != nil {
+		logger.Errorf("error updating point: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error updating point")
+		return
+	}
+
+	sendSuccess(ctx, http.StatusOK, "update-point", point)
+}
