1550f8001c1c554389a45a3f8c49e4f728c653f6
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-09 14:31:22 -0300

    :sparkles: add list points handler logic

---
+++ handler/point/listPoints.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/LuisMarchio03/acim-backend/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+// @BaseParh /api/v1
+
+// @Summary List Points
+// @Description List all Points
+// @Tags Point
+// @Accept json
+// @Producer json
+// @Success 200 {object} ListPointsResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /points [get]
+func ListPointsHandler(ctx *gin.Context) {
+	points := []schemas.Point{}
+
+	if err := db.Find(&points).Error; err != nil {
+
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error listing points: %v", err))
+		return
+	}
+
+	sendSuccess(ctx, http.StatusOK, "list-points", points)
+}
