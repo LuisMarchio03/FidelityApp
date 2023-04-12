d06c0bd2e6565e75a74417c549c8bcf9ac376c24
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-12 11:53:06 -0300

    :sparkles: add auth swagger on create point handler

--- handler/point/createPoint.go
+++ handler/point/createPoint.go
@@ -14,6 +14,8 @@ import (
 // @Tags Point
 // @Accept json
 // @Producer json
+// @Security ApiKeyAuth
+// @param Authorization header string true "Authorization"
 // @Param request body CreatePointRequest true "Request body"
 // @Success 201 {object} CreatePointResponse
 // @Failure 400 {object} ErrorResponse
