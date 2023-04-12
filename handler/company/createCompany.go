74bc1e69ab3d345dcc374c3db6d75efadb98cfd1
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-12 11:52:23 -0300

    :sparkles: add auth swagger on create company handler

--- handler/company/createCompany.go
+++ handler/company/createCompany.go
@@ -14,6 +14,8 @@ import (
 // @Tags Company
 // @Accept json
 // @Producer json
+// @Security ApiKeyAuth
+// @param Authorization header string true "Authorization"
 // @Param request body CreateCompanyRequest true "Request body"
 // @Success 201 {object} CreateCompanyResponse
 // @Failure 400 {object} ErrorResponse
