94d32b680c5e36c2fdd2bfa077396e7481f0a616
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-12 11:52:47 -0300

    :sparkles: add auth swagger on show company handler

--- handler/company/showCompany.go
+++ handler/company/showCompany.go
@@ -15,6 +15,8 @@ import (
 // @Tags Company
 // @Accept json
 // @Producer json
+// @Security ApiKeyAuth
+// @param Authorization header string true "Authorization"
 // @Param id query string true "Company identification"
 // @Success 200 {object} ShowCompanyResponse
 // @Failure 404 {object} ErrorResponse
