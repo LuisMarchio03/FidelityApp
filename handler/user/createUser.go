74dd84635f7c170ee2f0a8d0ae3c56c346f5ecb9
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-13 10:50:08 -0300

    :bug: remove auth middleware

--- handler/user/createUser.go
+++ handler/user/createUser.go
@@ -14,8 +14,6 @@ import (
 // @Tags User
 // @Accept json
 // @Producer json
-// @Security ApiKeyAuth
-// @param Authorization header string true "Authorization"
 // @Param request body CreateUserRequest true "Request body"
 // @Success 201 {object} CreateUserResponse
 // @Failure 400 {object} ErrorResponse
